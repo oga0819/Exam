cb1c95ad942073d3f39d4c9245de9b7e499e4ebd
o＿hiromichi <ohiromichi@LAPTOP-CSSV8TR4>
2025-07-02 14:47:46 +0900

    プロジェクト新規作成

--- src/scoremanager/SubjectDeleteAction.java
+++ src/scoremanager/SubjectDeleteAction.java
@@ -1,5 +1,29 @@
 package scoremanager;
 
-public class SubjectDeleteAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import bean.School;
+import bean.Subject;
+import dao.SubjectDAO;
+import tool.Action;
+
+public class SubjectDeleteAction extends Action {
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // ログイン中の学校情報を取得
+        School school = (School) request.getSession().getAttribute("school");
+
+        // リクエストパラメータからcdを取得
+        String cd = request.getParameter("cd");
+
+        // 科目情報を取得
+        SubjectDAO dao = new SubjectDAO();
+        Subject subject = dao.get(cd, school);
+
+        // リクエストに科目を保存
+        request.setAttribute("subject", subject);
+
+        // 確認ページへ
+        return "subject_delete.jsp";
+    }
 }
--- src/scoremanager/SubjectDeleteExecuteAction.java
+++ src/scoremanager/SubjectDeleteExecuteAction.java
@@ -1,5 +1,38 @@
-package scoremanager;
+package  scoremanager;
 
-public class SubjectDeleteExecuteAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import bean.School;
+import bean.Subject;
+import dao.SubjectDAO;
+import tool.Action;
+
+public class SubjectDeleteExecuteAction extends Action {
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // ログイン中の学校情報を取得
+        School school = (School) request.getSession().getAttribute("school");
+
+        // 削除対象のCDを取得
+        String cd = request.getParameter("cd");
+
+        // Subjectインスタンスを作成
+        Subject subject = new Subject();
+        subject.setCd(cd);
+        subject.setSchool(school);
+
+        // 削除処理
+        SubjectDAO dao = new SubjectDAO();
+        boolean success = dao.delete(subject);
+
+        // メッセージを設定
+        if (success) {
+            request.setAttribute("message", "科目を削除しました。");
+        } else {
+            request.setAttribute("message", "科目の削除に失敗しました。");
+        }
+
+        // リダイレクト先を指定（一覧へ戻るなど）
+        return "subject_result.jsp";
+    }
 }
